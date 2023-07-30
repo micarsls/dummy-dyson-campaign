5341c8023fb519302ab10d82d8a9a182c643fec5
Mica <48641666+micarsls@users.noreply.github.com>
2023-07-30 15:16:27 +0400

    final

--- scripts/config.js
+++ scripts/config.js
@@ -5,9 +5,11 @@ tailwind.config = {
         //black: "#1d1b1b",
         white: "#f5f5f5",
         copper: "#B47A4F",
-        blue: "#2E324E",
-        lightblue: "#40486A",
+        blue: "#40486A",
         lightgray: "#F8F8F8",
+      },
+      fontFamily: {
+        sans: ["Helvetica Neue, Helvetica", "Arial", "sans-serif"],
       },
     },
   },
--- scripts/custom.js
+++ scripts/custom.js
@@ -1,14 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Timer Initialization
   const fiveMinutes = 60 * 20;
   const display = document.querySelector("#countdown");
   startTimer(fiveMinutes, display);
 
+  // Splide Slider
+  const splide = new Splide(".splide", {
+    type: "fade",
+    rewind: true,
+    autoplay: true,
+    interval: 3000,
+    pauseOnHover: true,
+    pagination: false,
+    classes: {
+      arrow: "splide__arrow bg-copper",
+    },
+  });
+  splide.mount();
+
+  // Accordion Display
+  const accordions = document.querySelectorAll(".accordion");
+  accordions.forEach((accordion) => {
+    accordion.addEventListener("click", () => {
+      accordion.classList.toggle("active");
+      const panel = accordion.nextElementSibling;
+      panel.style.display = panel.style.display === "block" ? "none" : "block";
+    });
+  });
+
+  // Modal Close
   const modal = document.getElementById("modal");
   const closeModalButton = document.getElementById("closeModal");
   closeModalButton.addEventListener("click", () => {
     modal.classList.add("hidden");
   });
 
+  //Contact Form Submission
   const contact = document.getElementById("contact");
   contact.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -17,6 +44,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+//Timer Function
 const startTimer = (duration, display) => {
   let timer = duration;
   let minutes, seconds;
